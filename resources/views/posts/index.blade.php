5c9cec5c48b7bdf616291fe9feea32d6c8dea487
Luigi <luigismilzo@live.com>
2020-06-23 16:18:22 +0200

    Added fake comments print in archive page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,10 +5,24 @@
 
     @foreach($posts as $post)
         <article>
-            <h3>{{ $post->title }}</h3>
-            <h4>Author: {{ $post->user['name'] }}</h4>
-            <h4>Last edited: {{ $post->updated_at }}</h4>
-            <p>{{ $post->content }}</p>
+            <div>
+                <h3>{{ $post->title }}</h3>
+                <h4>Author: {{ $post->user['name'] }}</h4>
+                <h4>Last edited: {{ $post->updated_at }}</h4>
+                <p>{{ $post->content }}</p>
+            </div>
+
+            <div>
+                <h4>Comments</h4>
+                @forelse ($post->comments as $comment)
+                    <div>
+                        <strong>{{ $comment['author'] }}</strong> - <small><em>{{ $comment['created_at'] }}</em></small>
+                        <p>{{ $comment['content'] }}</p>
+                    </div>
+                @empty
+                    <em>No comments to show</em>
+                @endforelse
+            </div>
         </article>
         @if (!$loop->last)
             <hr>
